0f1a39de7a8e11620492f47fcfe7a17e480f4369
uraniumcore238 <uraniumcore238@gmail.com>
2022-07-28 08:59:53 +0300

    added simple tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,37 +1,37 @@
 class TestSimple:
 
 
-    def simple_positive_test(self):
+    def test_simple_positive(self):
         pass
 
 
-    def simple_positive_test2(self):
+    def test_simple_positive2(self):
         pass
 
 
-    def simple_positive_test3(self):
+    def test_simple_positive3(self):
         pass
 
 
-    def simple_positive_test4(self):
+    def test_simple_positive4(self):
         pass
 
 
-    def simple_positive_test5(self):
+    def test_simple_positive5(self):
         pass
 
 
-    def simple_negative_test(self):
+    def test_simple_negative(self):
         assert False
 
 
-    def simple_negative_test2(self):
+    def test_simple_negative2(self):
         assert False
 
 
-    def simple_negative_test3(self):
+    def test_simple_negative3(self):
         assert False
 
 
-    def simple_negative_test4(self):
+    def test_simple_negative4(self):
         assert False
