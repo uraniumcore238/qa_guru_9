7ff0db8ddf5ee3e5c722ef9d26804fa2b60a552d
uraniumcore238 <uraniumcore238@gmail.com>
2022-07-28 08:51:22 +0300

    added simple tests

---
+++ tests/test_simple.py
@@ -0,0 +1,34 @@
+def simple_positive_test():
+    pass
+
+
+def simple_positive_test2():
+    pass
+
+
+def simple_positive_test3():
+    pass
+
+
+def simple_positive_test4():
+    pass
+
+
+def simple_positive_test5():
+    pass
+
+
+def simple_negative_test():
+    assert False
+
+
+def simple_negative_test2():
+    assert False
+
+
+def simple_negative_test3():
+    assert False
+
+
+def simple_negative_test4():
+    assert False
